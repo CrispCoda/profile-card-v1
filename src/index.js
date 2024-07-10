f12002f60f1d28f61dd5a4a6410d16d3de431535
CrispCoda <joshua.p.crisp@outlook.com>
2024-07-10 15:51:29 +0100

    Creating Project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList skill="React" />
+      </div>
+    </div>
+  );
+}
+
+const Avatar = () => <img className="avatar" src="josh.jpg" alt="josh" />;
+
+const Intro = () => (
+  <div>
+    <h1>Joshua Crisp</h1>
+    <p>
+      I have been passionate about technology since a young age. I have had
+      various roles in Hospitality & Sales before landing an apprenticeship at
+      Glean in December 2021. This feels like the start of my journey, upon
+      completing my apprenticeship I have plans to start learning how to code
+      and develop software.
+    </p>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const SkillList = () => {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="😊" bgcolor="red" />
+      <Skill skill="HTML" emoji="😂" bgcolor="blue" />
+      <Skill skill="CSS" emoji="🤣" bgcolor="yellow" />
+      <Skill skill="JavaScript" emoji="❤️" bgcolor="green" />
+      <Skill skill="TypeScript" emoji="😍" bgcolor="turquoise" />
+    </div>
+  );
+};
+
+const Skill = (props) => {
+  return (
+    <div style={{ backgroundColor: props.bgcolor }} className="skill">
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
